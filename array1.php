87a990665b7d4b732f5dc0679fad37e9cfb9bfd5
guoatty40 <69834365+guoatty40@users.noreply.github.com>
2020-12-28 11:18:19 +0700

    Add files via upload

---
+++ array1.php
@@ -0,0 +1,12 @@
+<?php
+    $car1 = "Volvo";
+    $car1 = "BMW";
+    $car1 = "Toyota";
+    echo $car1."<br />";
+    echo $car2."<br />";
+    echo $car3."<br />";
+
+    $car = array("Volvo","BMW","Toyota");
+    echo "Member of Car = " count($car),"<hr />";
+    echo $car[0],"<br />",$car[1],"<br />",$car[2];
+?>
